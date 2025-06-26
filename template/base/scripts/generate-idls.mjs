4feb140c292aa0ba4618b05102758bd77a51b002
Loris Leiva <loris.leiva@gmail.com>
2025-06-26 14:07:59 +0100

    Stop using shank-js for building Anchor IDLs

--- template/base/scripts/generate-idls.mjs
+++ template/base/scripts/generate-idls.mjs
@@ -5,18 +5,25 @@ import { getCargo, getProgramFolders } from './utils.mjs';
 
 const binaryInstallDir = path.join(__dirname, '..', '.cargo');
 
-getProgramFolders().forEach((folder) => {
+for (const folder of getProgramFolders()) {
   const cargo = getCargo(folder);
   const isShank = Object.keys(cargo.dependencies).includes('shank');
   const programDir = path.join(__dirname, '..', folder);
+  const programName = cargo.package.name.replace(/-/g, '_');
+  const programId = cargo.package.metadata.solana['program-id'];
 
-  generateIdl({
-    generator: isShank ? 'shank' : 'anchor',
-    programName: cargo.package.name.replace(/-/g, '_'),
-    programId: cargo.package.metadata.solana['program-id'],
-    idlDir: programDir,
-    idlName: 'idl',
-    programDir,
-    binaryInstallDir,
-  });
-});
+  if (!isShank) {
+    await $`anchor build --idl ${programDir}`;
+    await $`mv ${programDir}/${programName}.json ${programDir}/idl.json`;
+  } else {
+    generateIdl({
+      generator: 'shank',
+      programName,
+      programId,
+      idlDir: programDir,
+      idlName: 'idl',
+      programDir,
+      binaryInstallDir,
+    });
+  }
+}
